835be1ca478ca131b18815b7f9ef529f4ffe7655
Mr-Jayed <jayedac1912@gmaik.com>
2026-02-13 23:36:25 +0600

    commit donez

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'app.dart';
+import 'app.dart'; // Ensure this file defines TrackOnApp
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // 3. Supabase Setup
   await Supabase.initialize(
-    url:'https://bmynlzludgipzbunndrh.supabase.co',
+    url: 'https://bmynlzludgipzbunndrh.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJteW5semx1ZGdpcHpidW5uZHJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxODY0MTUsImV4cCI6MjA3MTc2MjQxNX0.EH3FlCPNVOGRKsMGqVC3jJ_uwrgiJAL5bxIiXQI2OtU',
   );
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("e81fbf1a-1e59-4bc3-8254-1f738607b947");
+  OneSignal.Notifications.requestPermission(true);
 
-  FlutterError.onError = (details) {
-    FlutterError.presentError(details);
-    debugPrint("Global Error: ${details.exception}");
-  };
+  final currentUser = Supabase.instance.client.auth.currentUser;
+  if (currentUser != null) {
+    // If user is already logged in, make sure OneSignal knows it
+    OneSignal.login(currentUser.id);
+  }
 
   runApp(const TrackOnApp());
 }
